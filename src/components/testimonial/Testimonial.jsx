dc6d7349fe6c2bbc10f6d0092726b0a608a4104f
unknown <puneetsingh91100@gmail.com>
2025-08-05 22:43:53 +0530

    7th commit

--- src/components/testimonial/Testimonial.jsx
+++ src/components/testimonial/Testimonial.jsx
@@ -1,40 +1,42 @@
 import React from 'react'
 import './Testimonial.css'
-import  AVTR1 from  '../../assets/avt1.png'
-import  AVTR2 from  '../../assets/avt2.png'
-import  AVTR3 from  '../../assets/avt4.png'
+import AVTR1 from '../../assets/avt1.png'
+import AVTR2 from '../../assets/avt2.png'
+import AVTR3 from '../../assets/avt4.png'
 
 // core version + navigation, pagination modules:
-import { Pagination } from 'swiper/modules';
+import { A11y, Pagination, Navigation } from 'swiper/modules';
 
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 // Import Swiper styles
 import 'swiper/css';
+import 'swiper/css/navigation';
+
 import 'swiper/css/pagination';
 
 
-const data=[
+const data = [
   {
-    id:1,
-    image:AVTR1,
-    name:'Mrs River',
-    review:'"Puneet did an amazing job on our website’s UI. The design is clean, modern, and responsive across all devices. Communication was smooth, and every detail we requested was implemented perfectly."',
-    
+    id: 1,
+    image: AVTR1,
+    name: 'Mrs River',
+    review: '"Puneet did an amazing job on our website’s UI. The design is clean, modern, and responsive across all devices. Communication was smooth, and every detail we requested was implemented perfectly."',
+
   },
   {
-    id:2,
-    image:AVTR2,
-    name:'Mr Akshay Sachdeva, Bangalore',
-    review:'"Absolutely loved the portrait! The attention to facial details and shading brought the artwork to life. It now hangs proudly in my living room."',
-    
+    id: 2,
+    image: AVTR2,
+    name: 'Mr Akshay Sachdeva, Bangalore',
+    review: '"Absolutely loved the portrait! The attention to facial details and shading brought the artwork to life. It now hangs proudly in my living room."',
+
   },
   {
-    id:3,
-    image:AVTR3,
-    name:'Nisha K., Pune',
-    review:'"The custom artwork I received was unique and full of character. You can tell it’s made with heart. Will definitely order again!"',
-    
+    id: 3,
+    image: AVTR3,
+    name: 'Nisha K., Pune',
+    review: '"The custom artwork I received was unique and full of character. You can tell it’s made with heart. Will definitely order again!"',
+
   }
 ]
 const Testimonial = () => {
@@ -43,26 +45,28 @@ const Testimonial = () => {
       <h5>Review From Client</h5>
       <h2>Testimonials</h2>
 
-      <Swiper className="container testimonials__container" 
-      modules={[ Pagination]}
-      spaceBetween={40}
-      slidesPerView={1}
-      pagination={{ clickable: true }}
+      <Swiper className="container testimonials__container"
+        modules={[Navigation, Pagination, A11y]}
+        spaceBetween={40}
+        slidesPerView={1}
+        navigation={true}
+        pagination={{ clickable: true }}
       >
         {
-          data.map(({id,image,name, review})=>{
-            return(
+          data.map(({ id, image, name, review }) => {
+            return (
               <SwiperSlide key={id} className='testimonial'>
-          <div className="client__avatar">
-            <img src={image} alt='avtr1'/>
-            
-          </div>
-         <h5 className='client__name'>{name}</h5>
-            <small className='client__review'>{review}</small>
-        </SwiperSlide>
+                <div className="client__avatar">
+                  <img src={image} alt='avtr1' />
+
+                </div>
+                <h5 className='client__name'>{name}</h5>
+                <small className='client__review'>{review}</small>
+              </SwiperSlide>
             )
           })
         }
+        
       </Swiper>
     </section>
   )
